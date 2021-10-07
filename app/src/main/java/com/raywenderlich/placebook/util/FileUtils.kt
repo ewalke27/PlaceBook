3c1b34d322ad6a28e3aae7e55f4b3d965ba2f823
ewalke27 <90221957+ewalke27@users.noreply.github.com>
2021-10-07 12:21:35 -0400

    Ch 19 Complete and working!

---
+++ app/src/main/java/com/raywenderlich/placebook/util/FileUtils.kt
@@ -0,0 +1,10 @@
+import android.content.Context
+import java.io.File
+
+object FileUtils {
+    fun deleteFile(context: Context, filename: String) {
+        val dir = context.filesDir
+        val file = File(dir, filename)
+        file.delete()
+    }
+}
